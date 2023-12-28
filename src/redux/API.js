61315ca3d95206566930d55aaba59d41819f3b53
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-12-29 01:30:37 +0530

    changed base url

--- src/redux/API.js
+++ src/redux/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://renting-carnival.onrender.com/",
-  baseURL: " http://localhost:5000/",
+  baseURL: "https://renting-carnival.onrender.com/",
+  // baseURL: " http://localhost:5000/",
 });
 
 // http://localhost:5000/
